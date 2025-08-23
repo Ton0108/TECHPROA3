24e53cc33842298e7709da3ead66ddea73893c60
Ton0108 <ajanmapula@gmail.com>
2025-08-23 23:57:50 +0800

    Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("SEATWORKFIVE");
+
+        int countdown = 3;
+
+        while (countdown > 0) {
+            System.out.println(countdown);
+            countdown--;
+        }
+
+        System.out.println("Happy New Year!");
+        
+        System.out.println("SEATWORKSIX");
+
+        int dice = 1;
+
+        while (dice <= 6) {
+            if (dice < 6) {
+                System.out.println("No Yatzy.");
+            } else {
+                System.out.println("Yatzy!");
+            }
+
+            dice = dice + 1;
+        }
+      System.out.println("from Mr. Anthony Jan L. Mapula, he made this with the help of Miguel and youtube");
+
+    }
+}
 
