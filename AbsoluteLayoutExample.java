154d7e40702103b6263066ad8005c10224f5819a
Ton0108 <ajanmapula@gmail.com>
2026-02-26 04:44:42 +0800

    Create AbsoluteLayoutExample.java

---
+++ AbsoluteLayoutExample.java
@@ -0,0 +1,25 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class AbsoluteLayoutExample {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Absolute Positioning");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+
+        // Disable layout manager
+        frame.setLayout(null);
+
+        JButton btn1 = new JButton("Button 1");
+        btn1.setBounds(50, 50, 150, 40); // x, y, width, height
+
+        JButton btn2 = new JButton("Button 2");
+        btn2.setBounds(150, 120, 150, 40);
+
+        frame.add(btn1);
+        frame.add(btn2);
+
+        frame.setVisible(true);
+    }
+}
